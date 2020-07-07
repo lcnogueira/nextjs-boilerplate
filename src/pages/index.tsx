00acfc872286ec06965cd4d799c43c42ef2919ea
Luiz Claudio <luizcns18@gmail.com>
2020-07-07 08:12:30 -0300

    Set typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = "React Avançado" }: Props) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a>{title}</a>
         </h1>
 
         <p className="description">
@@ -54,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +209,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }

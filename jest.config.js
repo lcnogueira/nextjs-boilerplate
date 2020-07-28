3a625321a1a0f2a7de08c268aa4cc68030223d28
Luiz Claudio <luizcns18@gmail.com>
2020-07-28 00:29:18 -0300

    Update jest config
    Add src location by setting modulePaths on jest config. This way, jest will search this folder when resolving modules and will avoid an error if testing a component that uses absolute imports. Desconsider nextjs files (_app.tsx and _document.tsx) from code coverage as well.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
 }

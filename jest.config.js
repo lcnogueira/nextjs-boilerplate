92775f1de256bb5dc6a16757f4daa5ce19771387
Luiz Cláudio <luizcns18@gmail.com>
2021-02-07 06:46:52 -0300

    Add moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,9 @@ module.exports = {
     '!src/pages/_document.tsx'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  }
 }

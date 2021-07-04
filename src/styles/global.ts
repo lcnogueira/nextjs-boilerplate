8f3445c40886c60068f00e98298ea13028603514
Luiz Cláudio <luizcns18@gmail.com>
2021-07-04 10:10:56 -0300

    Fix box sizing on global theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,6 +34,11 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
   ${({ theme }) => css`

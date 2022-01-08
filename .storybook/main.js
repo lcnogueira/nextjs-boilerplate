d16ce5fda7655010a6ef7d3b60658343541c4718
Luiz Cláudio <luizcns18@gmail.com>
2022-01-08 01:37:10 -0300

    Update storybook to use webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
+  staticDirs: ['../public'],
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
-  babel: async (options) => ({
-    ...options,
-    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
-  }),
+  core: {
+    builder: 'webpack5'
+  },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
